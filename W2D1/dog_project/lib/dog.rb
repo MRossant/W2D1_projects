08eb3f1274eb9e61ae4d9c1e191145bccfd1cecb
mrossant <matteorossant@gmail.com>
2021-08-16 11:51:58 -0400

    Dog Project

---
+++ W2D1/dog_project/lib/dog.rb
@@ -0,0 +1,43 @@
+class Dog
+    def initialize(name, breed, age, bark, favorite_foods)
+        @name = name
+        @breed = breed
+        @age = age
+        @bark = bark
+        @favorite_foods = favorite_foods
+    end
+
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(num)
+        @age = num
+    end
+
+    def bark
+        if @age > 3
+            @bark.upcase
+        else
+            @bark.downcase
+        end
+    end
+
+    def favorite_foods
+        @favorite_foods
+    end
+
+    def favorite_food?(string)
+        favorite_foods[0].downcase!
+        favorite_foods[1].downcase!
+        favorite_foods.include?(string.downcase)
+    end
+end
