c10eeb79358b15ed3af6f542da1a40654a89b442
mrossant <matteorossant@gmail.com>
2021-08-20 13:48:35 -0400

    working on flight file

---
+++ W2D5/flight_project/lib/flight.rb
@@ -0,0 +1,14 @@
+require 'passenger'
+
+class Flight
+
+    attr_reader :passengers
+
+    def initialize(flight_number, capacity)
+        @flight_number = flight_number
+        @capacity = capacity
+        @passengers = []
+    end
+
+    
+end
