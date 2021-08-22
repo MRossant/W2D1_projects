d8727689e6f3f7400ef29fc624b9f48829780ca5
mrossant <matteorossant@gmail.com>
2021-08-21 20:53:14 -0400

    finished player file on battleship proj

--- W2D3/battleship_project/lib/player.rb
+++ W2D3/battleship_project/lib/player.rb
@@ -1,3 +1,8 @@
 class Player
-
+    def get_move
+        puts "enter a position with coordinates separated with a space like `4 7`"
+        response = gets.chomp.split(" ")
+        nums = response.map(&:to_i)
+        [nums[0], nums[1]]
+    end
 end
